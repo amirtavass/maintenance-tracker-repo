c2be952f241d296db971be43c0f6fe94da6781f5
amirtavass <amirtavass62@gmail.com>
2026-04-08 10:11:11 +0100

    Added contents of the User and Ticket schemas

---
+++ server/models/Ticket.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["open", "in-progress", "resolved"],
+      default: "open",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high", "urgent"],
+      default: "low",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Ticket", ticketSchema);
