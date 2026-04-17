83c25c59fdc4cb27321dd0da1561358243dc235c
amirtavass <amirtavass62@gmail.com>
2026-04-17 11:58:43 +0100

    added ticketroute to the routes and updated server.js and tested the endpoint in postman

---
+++ server/routes/ticketRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const {
+  createTicket,
+  updateTicketStatus,
+  getTicketById,
+  getTickets,
+} = require("../controllers/ticketController");
+
+const protect = require("../middleware/authMiddleware");
+const verifyRole = require("../middleware/verifyRole");
+
+const router = express.Router();
+
+router.use(protect);
+
+router
+  .route("/")
+  .get(getTickets)
+  .post(verifyRole("student", "staff", "admin"), createTicket);
+
+router.route("/:id").get(getTicketById);
+
+router
+  .route("/:id/status")
+  .patch(verifyRole("admin", "staff"), updateTicketStatus);
+
+module.exports = router;
